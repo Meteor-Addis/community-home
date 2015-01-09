3cfa92ac24b287294ca0bec9f05c0bbff884f372
hilawi <hilawibelachew@college.harvard.edu>
2015-01-09 14:10:28 +0300

    Tested github and meetup login. Implemented password login. Added change password and delete account functionality. Replaced accounts-ui with one made from scratch.

--- client/main.js
+++ client/main.js
@@ -7,7 +7,9 @@ Router.configure({
 });
 
 Router.route('/index.html', function() {
-    this.render('home');
+    this.render('home', {
+        data: function() { return Users.find();}
+    });
 });
 
 Router.route('/', function() {
@@ -15,29 +17,38 @@ Router.route('/', function() {
 });
 
 Router.route('/events.html', function() {
-    this.render('events');
+    this.render('events', {
+        data: function() { return Users.find();}
+    });
 });
 
 Router.route('/topics.html', function() {
-    this.render('topics');
+    this.render('topics', {
+        data: function() { return Users.find();}
+    });
 });
 
 Router.route('/blog.html', function() {
-    this.render('blog');
+    this.render('blog', {
+        data: function() { return Users.find();}
+    });
 });
 
 Router.route('/members.html', function() {
-    this.render('members');
+    this.render('members', {
+        data: function() { return Users.find({}, {sort: {"profile.name": 1}});},
+    });
 });
 
 Router.route('/event-detail.html', function() {
-    this.render('event-detail');
+    this.render('event-detail', {
+        data: function() { return Users.find();}
+    });
 })
 
 Router.route('user/:_id', function() {
     this.render('profile', {
-        data: function() { return Users.findOne({_id: this.params._id});},
-        allUsers: function() { return Users.find({});}
+        data: function() { return Users.findOne({_id: this.params._id});}
     });
 });
 
@@ -46,27 +57,188 @@ Router.route('user/:_id', function() {
 if (Meteor.isClient) {
     Meteor.subscribe("users");
 
-    Template.profile.helpers({
-        stuff: function() {
-            console.log(this._id);
-            return Users.findOne({_id: this._id}).profile.name;
-        }
-    });
+    //Meteor.autorun(function() {
+    //    var width = $('.profPic-container').width();
+    //    $('.profPic-container').css({'height':width+'px'});
+    //});
 
     Template.members.helpers({
         allUsers: function() {
-            console.log("Hi there");
-            console.log(Users.find().fetch());
-            return Users.find({});
+            return Users.find({}, {sort: {"profile.name": 1}});
+        },
+        facebookProfile: function() {
+            return this.services.facebook.id;
+        },
+        githubProfile: function() {
+            return this.profile.html_url;
+        },
+        meetupProfile: function() {
+            return this.profile.link;
+        }
+    });
+
+    Template.login.events({
+        "click #facebook-login": function(event) {
+            Meteor.loginWithFacebook({}, function(err) {
+                if (err) {
+                    throw new Meteor.Error("Facebook login failed");
+                }
+            });
+        },
+
+        "click #github-login": function(event) {
+            Meteor.loginWithGithub({
+                requestPermissions: ['user', 'public_repo']
+            },
+            function(err) {
+                if (err) {
+                    throw new Meteor.Error("Github login failed");
+                }
+            });
+        },
+
+        "click #meetup-login": function(event) {
+            Meteor.loginWithMeetup({}, function(err) {
+                if (err) {
+                    console.log(err);
+                    throw new Meteor.Error("Meetup login failed");
+                }
+            });
+        },
+
+        "click #signIn": function(event, template) {
+            event.preventDefault();
+
+            var email = template.find('.email-input').value;
+            var password = template.find('.password-input').value;
+
+            console.log(email, password);
+
+            Meteor.loginWithPassword(email, password, function(err) {
+                if (err) {
+                    throw new Meteor.Error("Password login failed");
+                }
+            });
+
+            return false;
+        },
+
+        "click #register": function(event) {
+            event.preventDefault();
+
+            $('.signInForm').css('display', 'none');
+            $('.registrationForm').css('display', 'block');
+
+            return false;
+        },
+
+        "click #cancelRegister": function(event) {
+            event.preventDefault();
+
+            $('.registrationForm').css('display', 'none');
+            $('.signInForm').css('display', 'block');
+
+            return false;
+        },
+
+        'click #createUser': function(event, template) {
+            event.preventDefault();
+
+            var first = template.find('.fName-input').value;
+            var last = template.find('.lName-input').value;
+            var fullName = first.concat(" ", last);
+            var email = template.find('#registrationEmail').value;
+            var password = template.find('#registrationPassword').value;
+
+
+            Accounts.createUser({email: email, password : password, profile: {firstName: first, lastName: last, name: fullName}}, function(err) {
+                if (err) {
+                    throw new Meteor.Error("Registration failed");
+                }
+
+            });
+
+            return false;
+        },
+
+        "click .changePassword": function(event, template) {
+            event.preventDefault();
+
+            $('.dropdown-menu').css('width', '300px');
+            $('#myProfile').css('display', 'none');
+            $('#changePassword').css('display', 'none');
+            $('#logout').css('display', 'none');
+            $('#changePasswordSection').css('display', 'block');
+
+            return false;
+        },
+
+        "click #submit-change": function(event, template) {
+            event.preventDefault();
+
+            var oldPassword = template.find('.old-password').value;
+            var newPassword = template.find('.new-password').value;
+            var confirmPassword = template.find('.confirm-password').value;
+
+            if (newPassword === confirmPassword) {
+                Accounts.changePassword(oldPassword, newPassword, function(err) {
+                    if (err) {
+                        alert(err.reason);
+                        throw new Meteor.Error("Password change failed");
+                    }
+                    else {
+                        alert("Password changed!");
+                        $('.dropdown-menu').css('width', 'auto');
+                        $('#myProfile').css('display', 'block');
+                        $('#changePassword').css('display', 'block');
+                        $('#logout').css('display', 'block');
+                        $('#changePasswordSection').css('display', 'none');
+                    }
+                });
+            }
+            else {
+                alert("Passwords don't match");
+            }
+
+            return false;
+        },
+
+        "click #cancel-change": function(event) {
+            event.preventDefault();
+
+            $('.dropdown-menu').css('width', 'auto');
+            $('#myProfile').css('display', 'block');
+            $('#changePassword').css('display', 'block');
+            $('#logout').css('display', 'block');
+            $('#changePasswordSection').css('display', 'none');
+
+            return false;
+        },
+
+        "click .deleteAccount": function(event) {
+            event.preventDefault();
+
+            var id = Meteor.userId();
+
+            Meteor.call('deleteUser', id);
+
+            Router.go('/index.html');
+
+        },
+
+        "click .form-control": function(event) {
+            event.preventDefault();
+            return false;
+        },
+
+        "click .logout": function(event) {
+            Meteor.logout(function(err) {
+                if (err) {
+                    throw new Meteor.Error("Logout failed");
+                }
+            })
         }
     })
-}
-
-if (Meteor.isServer) {
-    Meteor.publish("users", function() {
-        console.log(Users.find());
-        return Users.find();
-    });
 }
 
 Handlebars.registerHelper('activeIfCurrent', function(page) {
@@ -75,3 +247,4 @@ Handlebars.registerHelper('activeIfCurrent', function(page) {
     }
     return "";
 });
+
--- server/accounts.js
+++ server/accounts.js
@@ -2,10 +2,123 @@
  * Created by Hilawi on 1/1/15.
  */
 
+MEETUP_API_KEY = "78761a1837f20736a037068304565"; // This key is from the account :http://www.meetup.com/members/183336755/
+// The key should not be visible once deployed. It'll have to be placed in a file and passed to Meteor upon startup as an environment variable
+
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1549637801951392',
+    secret: '7551d29c678a324014f57276f79a726d'
+});
+
+ServiceConfiguration.configurations.remove({
+    service: "github"
+});
+ServiceConfiguration.configurations.insert({
+    service: "github",
+    clientId: "2c6034b39288c9a979af",
+    secret: "d482c0f02550cb2b8b0268909721502467dd8466"
+});
+
+ServiceConfiguration.configurations.remove({
+    service: "meetup"
+});
+ServiceConfiguration.configurations.insert({
+    service: "meetup",
+    clientId: "5g5feiednjd9j60oj7saqk2d3c",
+    secret: "3q9k06m0sjacm1usq93pp7jrnk"
+});
+
 Accounts.onCreateUser(function(options, user) {
-    if (options.profile) {
-        options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
-        user.profile = options.profile;
+    if (user.services) {
+        var service = _.keys(user.services)[0];
+        if (service == "facebook") {
+            if (options.profile) {
+                options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large&width=500&height=500";
+                user.profile = options.profile;
+            }
+            return user;
+        }
+
+        if (service == "github") {
+            // TODO: Figure out how to get user's full name and avatar picture from github. Same for service==meetup
+            var accessToken = user.services.github.accessToken,
+                result,
+                profile;
+
+            result = Meteor.http.get('https://api.github.com/user',{
+                params : {
+                    access_token : accessToken
+                },
+                headers: {"User-Agent": "Meteor/1.0"}
+            });
+
+            if(result.error){
+                console.log(result);
+                throw result.error
+            }
+
+            profile = _.pick(result.data,
+                'login',
+                'name',
+                'avatar_url',
+                'url',
+                'company',
+                'blog',
+                'location',
+                'email',
+                'bio',
+                'html_url'
+            );
+
+            profile.picture = profile.avatar_url;
+            delete profile.avatar_url;
+
+            user.profile = profile;
+
+            return user;
+        }
+
+        if (service == "meetup") {
+            var meetupId = user.services.meetup.id;
+            var target = 'https://api.meetup.com/2/member/' + meetupId + '?key=' + MEETUP_API_KEY + '&signed=true';
+
+            var result = HTTP.get(target, {
+                params: {
+                    format: 'json'
+                }
+            });
+
+            var userProperties = result.data;
+
+            if(userProperties.hasOwnProperty("photo") && userProperties.photo_link !== "") {
+                var picture = userProperties.photo.photo_link;
+            }
+
+            options.profile = {
+                'name': userProperties.name,
+                'link': userProperties.link,
+                'bio': userProperties.bio,
+                'picture': picture,
+                'id': meetupId
+            };
+
+            user.profile = options.profile;
+
+            return user;
+
+        }
+
+        if (service == "password") {
+            user.profile = options.profile;
+            user.profile.email = user.emails[0].address;
+
+            return user;
+        }
     }
     return user;
 });
